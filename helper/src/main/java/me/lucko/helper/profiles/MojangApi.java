a849dcc11e930d19ea94448d3e726325d33a2e70
ThePotatoDev <potatocoding@gmail.com>
2023-08-18 23:46:26 -0500

    dev

--- helper/src/main/java/me/lucko/helper/profiles/MojangApi.java
+++ helper/src/main/java/me/lucko/helper/profiles/MojangApi.java
@@ -25,63 +25,24 @@
 
 package me.lucko.helper.profiles;
 
-import com.google.common.collect.Lists;
-import com.google.gson.stream.JsonReader;
-
-import me.lucko.helper.Schedulers;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import me.lucko.helper.promise.Promise;
-import me.lucko.helper.utils.UndashedUuids;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.List;
-import java.util.Objects;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.text.MessageFormat;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Utilities for interacting with the Mojang API.
  */
-@Deprecated // API subject to change
 public final class MojangApi {
-    public static final String PROFILES_URL = "https://api.mojang.com/users/profiles/minecraft/%s";
-    public static final String NAME_HISTORY_URL = "https://api.mojang.com/user/profiles/%s/names";
-
-    /**
-     * Gets a promise for the online {@link UUID} of a player by fetching it from the Mojang API.
-     * The Mojang API has a limit of 600 requests per 10 minutes.
-     *
-     * @param username the username of the player from which to get the {@link UUID}
-     * @return a promise for the online {@link UUID} of the player with the supplied name
-     */
-    public static Promise<UUID> usernameToUuid(String username) {
-        Objects.requireNonNull(username, "username");
-        if (username.isEmpty()) {
-            throw new IllegalArgumentException("empty");
-        }
-
-        return Schedulers.async().supply(() -> {
-            try (JsonReader reader = new JsonReader(new InputStreamReader(new URL(String.format(PROFILES_URL, username)).openConnection().getInputStream()))) {
-                reader.beginObject();
-                reader.skipValue();
-                return UndashedUuids.fromString(reader.nextString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        });
-    }
-
-    /**
-     * Gets a promise of the online username of the player with the supplied {@link UUID} by fetching it from the Mojang API.
-     * The Mojang API has a limit of 600 requests per 10 minutes.
-     *
-     * @param uuid the {@link UUID} of the player from which to get the name
-     * @return a promise of the online username of the player with the supplied uuid, or null if it doesn't exists
-     */
-    public static Promise<String> uuidToUsername(UUID uuid) {
-        return getUsernameHistory(uuid).thenApplyAsync(names -> names.isEmpty() ? null : names.get(names.size() - 1));
-    }
+    public static final String PROFILE_HISTORY_URL = "https://sessionserver.mojang.com/session/minecraft/profile/{0}";
 
     /**
      * Gets a promise of the history of the names owned by the player with the supplied {@link UUID}, in chronological order, fetching it from the Mojang API.
@@ -90,31 +51,52 @@ public final class MojangApi {
      * @param uuid the {@link UUID} of the player from which to fetch the name history
      * @return a promise of a List String with all the names owned by the player with the supplied uuid, in chronological order
      */
-    public static Promise<List<String>> getUsernameHistory(UUID uuid) {
-        Objects.requireNonNull(uuid, "uuid");
+//    public static Promise<List<String>> getUsernameHistory(UUID uuid) {
+//        Objects.requireNonNull(uuid, "uuid");
+//
+//        return Schedulers.async().supply(() -> {
+//            List<String> names = Lists.newArrayList();
+//
+//            try (JsonReader reader = new JsonReader(new InputStreamReader(new URL(String.format(NAME_HISTORY_URL, UndashedUuids.toString(uuid))).openConnection().getInputStream()))) {
+//                reader.beginArray();
+//                for (int i = 0; reader.hasNext(); i++) {
+//                    reader.beginObject();
+//                    reader.skipValue();
+//                    names.add(reader.nextString());
+//                    if (i != 0) {
+//                        reader.skipValue();
+//                        reader.skipValue();
+//                    }
+//                    reader.endObject();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return names;
+//        });
+//    }
 
-        return Schedulers.async().supply(() -> {
-            List<String> names = Lists.newArrayList();
+    public static Promise<Optional<String>> uuidToUsername(UUID uuid) {
+        String url = MessageFormat.format(PROFILE_HISTORY_URL, uuid.toString());
 
-            try (JsonReader reader = new JsonReader(new InputStreamReader(new URL(String.format(NAME_HISTORY_URL, UndashedUuids.toString(uuid))).openConnection().getInputStream()))) {
-                reader.beginArray();
-                for (int i = 0; reader.hasNext(); i++) {
-                    reader.beginObject();
-                    reader.skipValue();
-                    names.add(reader.nextString());
-                    if (i != 0) {
-                        reader.skipValue();
-                        reader.skipValue();
-                    }
-                    reader.endObject();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
 
-            return names;
-        });
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JsonElement element = new JsonParser().parse(response.body());
+
+            System.out.println(element.toString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return Promise.completed(Optional.empty());
     }
+
 
     private MojangApi() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
